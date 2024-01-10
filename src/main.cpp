03b2f297b0352d0f02ee00f77c674bbdb1e639c1
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2024-01-10 14:12:08 +0100

    Project Base

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 
+#include "debug_interceptor.hpp"
+#include "CMakeConfig.hpp"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    auto debugInterceptor = Debug_Interceptor::getInstance();
+
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/SmartCity/Main.qml"_qs);
+
+    engine.rootContext()->setContextProperty("ROOT_PATH", ROOT_PATH);
+
+    const QUrl url(u"qrc:/SmartCity/src_gui/Main.qml"_qs);
+
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
         &app, [url](QObject *obj, const QUrl &objUrl) {
             if (!obj && url == objUrl)
