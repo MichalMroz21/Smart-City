14c0a041b336122c995cd74e40762806e2c893f5
Jakub <kubawierzba1@wp.pl>
2024-01-10 19:29:23 +0100

    Creating Path ADDEDD

--- howling_legs/lib/main.dart
+++ howling_legs/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:howling_legs/PathCreator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:geocoding/geocoding.dart';
@@ -91,6 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: const Text('Go to Address'),
             ),
           ),
+          PathCreator()
         ],
       ),
     );
