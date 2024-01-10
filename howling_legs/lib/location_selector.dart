61663967e16689f9b282107dc5cfd8e0db801163
MichalMroz21 <michal21mr@gmail.com>
2024-01-10 22:06:16 +0100

    wyszukiwarka

--- howling_legs/lib/location_selector.dart
+++ howling_legs/lib/location_selector.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LocationSelector extends StatelessWidget {
   static const List<String> _kOptions = <String>[
@@ -20,13 +22,41 @@ class LocationSelector extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Autocomplete<String>(
-            optionsBuilder: (TextEditingValue textEditingValue) {
-              if (textEditingValue.text == '') {
+            optionsBuilder: (TextEditingValue textEditingValue) async {
+
+              String search = textEditingValue.text;
+
+              if (search == '') {
                 return const Iterable<String>.empty();
               }
-              return _kOptions.where((String option) {
-                return option.contains(textEditingValue.text.toLowerCase());
-              });
+              
+              String url = "http://192.168.1.121:8080/search.php?q=" + search;
+
+              var request = Uri.parse(url);
+
+              var response = await http.get(request);
+
+              if (response.statusCode == 200) {
+
+                  String resp = response.body;
+
+                  // Parse the JSON string
+                  List<dynamic> jsonData = json.decode(resp);
+
+                  List<String> results = [];
+
+                  // Extract points from the JSON
+                  for(var data in jsonData){
+                    print(data);
+                      results.add(data["display_name"].toString());
+                  }
+
+                  Iterable<String> iter = results;
+                  
+                  return iter;
+              }
+
+              return const Iterable<String>.empty();
             },
             onSelected: (String selection) {
               debugPrint('You just selected $selection');
